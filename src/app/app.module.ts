5c5a9ee4683fdf45de7844f466c2917fcc6f77fa
Jerico De Guzman <jjddeguzman@gmail.com>
2020-09-01 15:25:52 +0800

    FEAT : Add forms module in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AngularComponent } from './angular/angular.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AngularComponent } from './angular/angular.component';
     AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
